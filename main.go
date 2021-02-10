062fda816a72752c9da0a713cbed726fa9394fed
Etai Lev Ran <etail@il.ibm.com>
2021-02-10 17:19:36 +0200

    comments and minor cleanup

--- main.go
+++ main.go
@@ -32,24 +32,38 @@ type (
 		} `json:"assets"`
 		Body string `json:"body"`
 	}
+
+	// GithubReleaseList is an array of GitHubRelease objects
+	GithubReleaseList []GithubRelease
 )
 
 const (
 	kernelImageName = "bzImage"
+	ghReposEndpoint = "https://api.github.com/repos/"
 )
 
 var (
 	emptySHA1 = fmt.Sprintf("%x", sha1.New().Sum(nil))
 
-	repo        = flag.String("github-repo", "nathanchance/WSL2-Linux-Kernel", "WSL2 kernel source repository")
-	kernelsDir  = flag.String("kernel-dir", "wsl2", "directory used for saving kernel images")
-	tag         = flag.Bool("tag", true, "auto-tag downloaded kernel using release.tag_name JSON property")
-	autoInstall = flag.Bool("install", false, "auto-install kernel to WSL2 (requires wsl reboot)")
-	// add an option to list releases for the repo? 'GET /repos/{owner}/{repo}/releases'
+	repo = flag.String("github-repo", "nathanchance/WSL2-Linux-Kernel",
+		"WSL2 kernel source repository on github")
+	kernelsDir = flag.String("kernels-dir", "wsl2-kernels",
+		"directory used for downloaded kernel image, overrides .wslconfig value when defined")
+	//tagged      = flag.String("-tag", "", "download a specific release based on its tag, instead of 'latest'")
+	autoTag     = flag.Bool("tag-image", true, "use 'release.tag_name' as image file extension")
+	autoInstall = flag.Bool("install", false, "auto-install kernel to WSL2 -- requires WSL reboot!")
+	listOnly    = flag.Bool("list", false, "list recent releases, without downloading anything")
 )
 
 func main() {
 	flag.Parse()
+
+	if *listOnly {
+		if err := listRecentReleases(); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
 
 	home, err := userHomeDirectory()
 	// wsl2Kernel, err := parseWSLConfig()
@@ -61,7 +75,10 @@ func main() {
 	}
 	log.Println("current kernel image", storedImage, "sha:", localDigest)
 
-	link := "https://api.github.com/repos/" + *repo + "/releases/latest"
+	// split the bzImage download (downloadKernelFromURL)
+	// from the link calculation (getLatestReleaseKernelURL)
+	// download a specific kernel if *tagged != nil
+	link := ghReposEndpoint + *repo + "/releases/latest"
 	kernel, remoteDigest, err := downloadReleasedImage(link)
 	if err != nil {
 		log.Fatal(err)
@@ -81,6 +98,8 @@ func main() {
 	}
 }
 
+// @TODO: split the GH release and their access to a separate file, same for wslconfig handling
+
 // download the kernel image from the given release into a temporary location.
 // Returns the downloaded image path, its SHA1 digest and any error that may have occurred.
 func downloadReleasedImage(link string) (string, string, error) {
@@ -98,17 +117,15 @@ func downloadReleasedImage(link string) (string, string, error) {
 		return "", emptySHA1, fmt.Errorf("failed to parse release %s: %w", link, err)
 	}
 
-	log.Printf("Latest release in %s tagged %s, published %v.\n",
-		link, release.TagName, release.PublishedAt)
+	log.Printf("Latest release in %s is %s, published %s (draft/prerelease:%t).\n",
+		*repo, release.TagName, release.PublishedAt.Format("2006-01-02"),
+		release.Draft || release.Prerelease)
 	log.Println("Description")
 	log.Println(release.Body)
-	if release.Draft || release.Prerelease {
-		return "", emptySHA1, fmt.Errorf("release %s marked draft/prerelease. Aborting", link)
-	}
 
 	for i := 0; i < len(release.Assets); i++ {
 		if release.Assets[i].Name == kernelImageName {
-			if *tag {
+			if *autoTag {
 				downloadedKernel = fmt.Sprintf("%s.%s", downloadedKernel, release.TagName)
 			}
 			downloadedKernel = path.Clean(downloadedKernel)
@@ -142,19 +159,19 @@ func downloadKernelImage(assetURL, localPath string) error {
 // return the SHA1 digest for the named file
 func sha1sum(fn string) (string, error) {
 	if _, err := os.Stat(fn); err != nil {
-		return fmt.Sprintf("%x", sha1.New().Sum(nil)), fmt.Errorf("failed to stat %s: %w", fn, err)
+		return emptySHA1, fmt.Errorf("failed to stat %s: %w", fn, err)
 	}
 
 	file, err := os.Open(fn)
 	if err != nil {
-		return fmt.Sprintf("%x", sha1.New().Sum(nil)), fmt.Errorf("failed to open %s: %w", fn, err)
+		return emptySHA1, fmt.Errorf("failed to open %s: %w", fn, err)
 	}
 	defer file.Close()
 
 	h := sha1.New()
 	_, err = io.Copy(h, file)
 	if err != nil {
-		return fmt.Sprintf("%x", sha1.New().Sum(nil)), fmt.Errorf("failed to checksum %s: %w", fn, err)
+		return emptySHA1, fmt.Errorf("failed to checksum %s: %w", fn, err)
 	}
 	return fmt.Sprintf("%x", h.Sum(nil)), nil
 }
@@ -177,8 +194,33 @@ func userHomeDirectory() (string, error) {
 	return u.HomeDir, nil
 }
 
+// list recent releases and their status
+func listRecentReleases() error {
+	releases := ghReposEndpoint + *repo + "/releases"
+	client := http.Client{Timeout: 5 * time.Second}
+	r, err := client.Get(releases)
+
+	if err != nil {
+		return err
+	}
+
+	recent := GithubReleaseList{}
+	err = json.NewDecoder(r.Body).Decode(&recent)
+	r.Body.Close()
+	if err != nil {
+		return err
+	}
+	log.Println("listing releases from", *repo)
+	for i := 0; i < len(recent); i++ {
+		log.Printf("release %s published %v (draft/pre-release:%t)",
+			recent[i].TagName, recent[i].PublishedAt.Format("2006-01-02"),
+			recent[i].Draft || recent[i].Prerelease)
+	}
+	return nil
+}
+
 // wsl2Kernel, err := parseWSLConfig()
 
 // parse <home>/.wslconfig if exists - use INI package
 // determine kernel dir (if not set via a flag). Use default if neither is defined
-// if there is a kernel key in the wsl2 section, use that for 'current'
+// if there is a kernel key in the wsl2 section, use that for 'current' digest
